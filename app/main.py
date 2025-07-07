f150eb38966b4c6a745cdb70e95a61fabbc26dec
vasylhladyshev404 <vasyl@nextgear.nl>
2025-07-07 16:45:08 +0200

    2

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8"
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
@@ -14,5 +15,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "status": "failed" if errors else "passed"
     }
 
+
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
+    return [format_single_linter_file(file_path, errors) for file_path,
+        errors in linter_report.items()]
